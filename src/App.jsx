c2390c376f3fddcf299e6321f370dd9a6f0e0a2c
finn-01 <nguyensam576@gmail.com>
2022-02-01 13:12:09 +0700

    Register.jsx pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-	return <Product />;
+	return <Register />;
 };
 
 export default App;
